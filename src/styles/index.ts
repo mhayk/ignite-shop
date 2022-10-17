24170e5058f2057e261fffb6a06596ad83ca346e
Mhayk Whandson <hi@mhayk.com>
2022-10-17 15:53:24 +0100

    feat: configure stitches to generate css and inject even javascript is not enable in client side

---
+++ src/styles/index.ts
@@ -0,0 +1,18 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+    config,
+    styled,
+    css,
+    globalCss,
+    keyframes,
+    getCssText,
+    createTheme
+} = createStitches({
+    theme: {
+        colors: {
+            purple : '#8257e6'
+        }
+    }
+})
+
