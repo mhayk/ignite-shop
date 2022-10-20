87717734b079b55252040db475f349e418f46533
Mhayk Whandson <hi@mhayk.com>
2022-10-20 15:29:33 +0100

    feat: create and style Header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
 
+import logoImg from '../assets/logo.svg';
+import Image from 'next/image';
+import { Container, Header } from '../styles/pages/app';
+
 globalStyles()
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
