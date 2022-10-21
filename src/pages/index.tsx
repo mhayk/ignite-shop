d4655884fb890eff54fcc976af36751fa82c9025
Mhayk Whandson <hi@mhayk.com>
2022-10-21 11:38:10 +0100

    feat: fetch products from Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Image from 'next/future/image'
+import Stripe from 'stripe'
 
 import { useKeenSlider } from 'keen-slider/react'
 
@@ -10,8 +11,18 @@ import shirt2 from '../assets/shirts/2.png'
 import shirt3 from '../assets/shirts/3.png'
 
 import 'keen-slider/keen-slider.min.css'
+import { stripe } from '../lib/stripe'
 
-const Home: NextPage = () => {
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: number;
+  }[]
+}
+
+export default function Home({products}: HomeProps) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
@@ -20,36 +31,41 @@ const Home: NextPage = () => {
   })
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
-      <Product className='keen-slider__slide'>
-        <Image src={shirt1} width={520} height={480} alt="" />
-        <footer>
-          <strong>Shirt X</strong>
-          <span>£79.90</span>
-        </footer>
-      </Product>
-      <Product className='keen-slider__slide'>
-        <Image src={shirt2} width={520} height={480} alt="" />
-        <footer>
-          <strong>Shirt X</strong>
-          <span>£79.90</span>
-        </footer>
-      </Product>
-      <Product className='keen-slider__slide'>
-        <Image src={shirt3} width={520} height={480} alt="" />
-        <footer>
-          <strong>Shirt X</strong>
-          <span>£79.90</span>
-        </footer>
-      </Product>
-      <Product className='keen-slider__slide'>
-        <Image src={shirt3} width={520} height={480} alt="" />
-        <footer>
-          <strong>Shirt X</strong>
-          <span>£79.90</span>
-        </footer>
-      </Product>
+
+      {products.map(product => {
+        return (
+          <Product className='keen-slider__slide' key={product.id}>
+            <Image src={product.imageUrl} width={520} height={480} alt="" placeholder='blur' />
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        )
+      })}
+
     </HomeContainer>
   )
 }
 
-export default Home
+export const getServerSideProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
+
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: price.unit_amount / 100,
+    }
+  })
+  console.log(response.data)
+  return {
+    props: {
+      products
+    }
+  }
+}
