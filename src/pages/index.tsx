5df1e7793ba8657bd88a79349253589470efb723
Mhayk Whandson <hi@mhayk.com>
2022-10-21 06:23:35 +0100

    feat: create the carrossel with keen-slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,48 @@
 import type { NextPage } from 'next'
 import Image from 'next/future/image'
+
+import { useKeenSlider } from 'keen-slider/react'
+
 import { HomeContainer, Product } from '../styles/pages/home'
 
 import shirt1 from '../assets/shirts/1.png'
 import shirt2 from '../assets/shirts/2.png'
 import shirt3 from '../assets/shirts/3.png'
 
+import 'keen-slider/keen-slider.min.css'
 
 const Home: NextPage = () => {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  })
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className='keen-slider__slide'>
         <Image src={shirt1} width={520} height={480} alt="" />
         <footer>
           <strong>Shirt X</strong>
           <span>£79.90</span>
         </footer>
       </Product>
-      <Product>
+      <Product className='keen-slider__slide'>
         <Image src={shirt2} width={520} height={480} alt="" />
+        <footer>
+          <strong>Shirt X</strong>
+          <span>£79.90</span>
+        </footer>
+      </Product>
+      <Product className='keen-slider__slide'>
+        <Image src={shirt3} width={520} height={480} alt="" />
+        <footer>
+          <strong>Shirt X</strong>
+          <span>£79.90</span>
+        </footer>
+      </Product>
+      <Product className='keen-slider__slide'>
+        <Image src={shirt3} width={520} height={480} alt="" />
         <footer>
           <strong>Shirt X</strong>
           <span>£79.90</span>
