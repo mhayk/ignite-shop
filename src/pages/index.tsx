eafa5420f364b2c976c861640d87ed8985d97363
Mhayk Whandson <hi@mhayk.com>
2022-10-26 09:10:51 +0100

    feat: turn off prefetch in Link component from nextjs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,6 +34,7 @@ export default function Home({products}: HomeProps) {
           <Link
             key={product.id}
             href={`/product/${product.id}`}
+            prefetch={false}
           >
             <Product className='keen-slider__slide' >
               <Image src={product.imageUrl} width={520} height={480} alt="" />
