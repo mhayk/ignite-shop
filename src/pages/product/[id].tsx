bcc44923de9f9dba144541936684052bae4efd04
Mhayk Whandson <hi@mhayk.com>
2022-10-26 05:56:54 +0100

    feat: create function getStaticPaths to return the list of products to be created the static page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,22 +1,75 @@
+import { GetStaticPaths, GetStaticProps } from "next"
+import Image from "next/future/image"
 import { useRouter } from "next/router"
+import Stripe from "stripe"
+import { stripe } from "../../lib/stripe"
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
-export default function Product() {
-    const { query} = useRouter()
+interface ProductProps {
+    product: {
+        id: string;
+        name: string;
+        imageUrl: string;
+        price: string;
+        description: string;
+    }
+}
+
+export default function Product({ product }: ProductProps) {
+    const { query } = useRouter()
+
     return (
         <ProductContainer>
             <ImageContainer>
-
+                <Image src={product.imageUrl} width={520} height={480} alt="" />
             </ImageContainer>
 
             <ProductDetails>
-                <h1>Shirt X</h1>
-                <span>£ 79.90</span>
+                <h1>{product.name}</h1>
+                <span>{product.price}</span>
 
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, aperiam animi? Fuga ducimus debitis culpa deleniti cumque in, doloribus iste aperiam repudiandae cum vero vitae nam harum officiis! Saepe, deleniti.</p>
+                <p>{product.description}</p>
 
                 <button>BUY NOW</button>
             </ProductDetails>
         </ProductContainer>
     )
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [
+            { params: { id: 'prod_MeXBe4sI9djRrC'}}
+        ],
+        fallback: false,
+    }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+    const productId = params.id
+
+    const product = await stripe.products.retrieve(productId, {
+        expand: ['default_price']
+    })
+
+    const price = product.default_price as Stripe.Price
+
+
+
+    return {
+        props: {
+            product: {
+                id: product.id,
+                name: product.name,
+                imageUrl: product.images[0],
+                price: new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'GBP'
+                }).format(price.unit_amount ? (price.unit_amount / 100) : 0),
+                description: product.description
+            }
+        },
+        revalidate: 60 * 60 * 1, // 1 hour
+    }
+}
+
